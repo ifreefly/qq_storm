7f36a33f412a4c292024c6727cfd7a07bcd49b5d
idevcod@gmail.com <dell@dell-Inspiron-1464.(none)>
2013-10-06 11:19:07 +0800

    获取纯正的链接，允许从外部输入分享链接

--- grab_link.py
+++ grab_link.py
@@ -4,19 +4,26 @@
 import urllib2;
 import re; 
 import os;
+import time;
 
-src_url='http://fenxiang.qq.com/share/index.php/share/share_c/index/3SyKv1dyd~FIc1OKSq7Sz~8UIdPlo3AyiiY';
-re_string='qhref="(?P<url>.*?)"';
-re_title='<h1>(?P<title>.*?)</h1>';
-req=urllib2.urlopen(src_url);
-resp=req.read();
-found=re.finditer(re_string,resp);
-title_found=re.search(re_title,resp);
-savepath=title_found.group('title')+".txt";
-#print savepath;
-#print found.group('url');
-with open(savepath,'a+') as save_file:
-  for url in found:
-    save_file.write(url.group());
-    save_file.write('\n');
+#src_url='http://fenxiang.qq.com/share/index.php/share/share_c/index/3SyKv1dyd~FIc1OKSq7Sz~8UIdPlo3AyiiY';
+def get_content(src_url):
+  re_string='qhref="(?P<url>.*?)"';
+  re_title='<h1>(?P<title>.*?)</h1>';
+  req=urllib2.urlopen(src_url);
+  resp=req.read();
+  found=re.finditer(re_string,resp);
+  title_found=re.search(re_title,resp);
+  savepath=title_found.group('title')+".txt";
+  #print savepath;
+  #print found.group('url');
+  if os.path.isfile(savepath):
+    savepath=title_found.group('title')+time.strftime('%Y%m%d%H%I%M%S',time.localtime(time.time()))+".txt"
+  with open(savepath,'a+') as save_file:
+    for url in found:
+      save_file.write(url.group('url'));
+      save_file.write('\n');
     
+print "请输入链接："
+src_url=str(raw_input(u"url:"));
+get_content(src_url);
